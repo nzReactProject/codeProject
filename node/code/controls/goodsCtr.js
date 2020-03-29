8b39f833fa3b5d7cf120a1f527f3e0ad8dd2c2d6
Shaozia <1587142527@qq.com>
2020-03-29 16:12:18 +0800

    接口完成

--- node/code/controls/goodsCtr.js
+++ node/code/controls/goodsCtr.js
@@ -8,8 +8,29 @@ let goodsList = async () => {
 }
 /***  往下写...    */
 
+// 这里是往数据库添加商品信息的方法
+// 这里的obj形参接收的是当调用商品添加这个方法，传递过来的所要添加的商品信息
+let goodsAdd = async (obj) => { 
+  // 使用商品数据库模型下的insetMany方法来将在页面获取到的所需添加商品的信息添加到数据库中
+  let result = await goodsModel.insertMany(obj)
+}
+
+// 删除商品方法
+// 删除商品会需要一个参数，这个参数就是商品的id，所以在这里接收id参数
+let goodsDel = async (_id) => {
+  // 使用商品模型下到的deleteOne方法，根据商品传递过来的id进行数据库商品的删除
+  let result = await goodsModel.deleteOne( {_id} )
+}
 
 
+// 这里是修改商品信息的方法
+// 修改商品的信息，是根据商品的id去决定修改的商品是哪一个
+// 还要拿到被修改商品的修改信息
+// _id是对应需要修改的商品信息，updateInfo是商品需要修改的修改信息
+let goodsUpdate = async (_id,updateInfo) => {
+  // 使用商品数据模型下的updateOne方法进行修改
+  let result = await goodsModel.updateOne({_id},updateInfo)
+}
 
 // 将方法抛出
-module.exports = {goodsList}
+module.exports = { goodsList,goodsAdd,goodsDel,goodsUpdate }
--- node/code/router/goodsRouter.js
+++ node/code/router/goodsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {goodsList} = require('../controls/goodsCtr')
+const { goodsList,goodsAdd,goodsDel,goodsUpdate } = require('../controls/goodsCtr')
 
 /**
  * @api {get} /goods/list   查询所有的商品
@@ -19,7 +19,71 @@ router.get('/list',(req,res)=>{
     res.send({code:0,msg:'查询失败',err})
   })
 })
-// 红红火火恍恍惚惚
 
 
+// 这里是添加商品信息接口
+/**
+ * @api {post} /goods/add   添加商品接口
+ * @apiName add
+ * @apiGroup list
+ *
+ * @apiSuccess {String} code 状态码r.
+ * @apiSuccess {String} msg  信息提示.
+ * 
+ */
+router.post('/add',(req,res) => {
+  let { name,price,stock,img,desc,putaway,kind } = req.body
+  goodsAdd( { name,price,stock,img,desc,putaway,kind } ).then((data) => {
+    res.send({code:1,msg:'添加成功'})
+  }).catch(() => {
+    res.send({code:0,msg:'添加失败'})
+  })
+})
+
+// 这里是删除商品接口
+/**
+ * @api {delete} /goods/del   删除商品接口
+ * @apiName del
+ * @apiGroup list
+ *
+ * @apiSuccess {String} code 状态码r.
+ * @apiSuccess {String} msg  信息提示.
+ * 
+ */
+router.delete('/del',(req,res) => {
+  // 获取到页面用户选择的商品id
+  let {_id} = req.body;
+  // console.log({_id})
+  // 调用商品的删除方法，对数据库里的商品信息进行删除
+  goodsDel({_id}).then((data) => {
+    res.send({code:1,msg:'删除成功'})
+  }).catch((err) => {
+    console.log(err)
+    res.send({code:0,msg:'删除失败'})
+  })
+})
+
+
+// 这里是修改商品信息接口
+/**
+ * @api {post} /goods/update   修改商品接口
+ * @apiName update
+ * @apiGroup list
+ *
+ * @apiSuccess {String} code 状态码r.
+ * @apiSuccess {String} msg  信息提示.
+ * 
+ */
+router.post('/update',(req,res) => {
+  console.log(req.body)
+  // 获取用户在页面选择的商品id以及要修改商品的商品信息
+  let {_id,name,price,stock,img,desc,putaway,kind} = req.body
+  // 调用修改商品信息的方法
+  goodsUpdate(_id,{name,price,stock,img,desc,putaway,kind}).then(() => {
+    res.send({code:1,msg:'修改成功'})
+  }).catch((err) => {
+    console.log(err)
+    res.send({code:0,msg:'修改失败'})
+  })
+})
 module.exports = router
